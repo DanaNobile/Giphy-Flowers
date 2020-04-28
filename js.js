2a793e4eefa42eb15729f7402d17292bdae5815a
Dana Nobile <danagnobile@gmail.com>
2020-04-28 15:19:56 -0400

    Added input box and submit buton

---
+++ js.js
@@ -0,0 +1,16 @@
+
+
+
+
+$(document).ready(function () {
+    //this is an array of exercises
+    let exercises = ["back", "chest", "glute", "bicep", "tricep", "calf", "quad", "ab"]
+
+    // this function makes buttons and then adds them on to the page
+
+    function buttons
+
+for (let i =)
+
+
+});
