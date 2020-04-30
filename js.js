7d40285536eff8dacad7ac9fdc8fb9d64ec4d89c
Dana Nobile <danagnobile@gmail.com>
2020-04-29 21:59:07 -0400

    Created function to display input field content to page and append to existing array

--- js.js
+++ js.js
@@ -21,12 +21,9 @@ let APIKEY = "AO19ApHDYRwaLBR55kjmQCddKIcDrAKy";
 //this is an array of flowers
 var flowers = ["Roses", "Lillies", "Gardenias", "Daisies", "Orchids", "Sunflowers", "Tulips", "Peonies"];
 
-// Function for displaying movie data
-function renderButtons() {
+// Function for displaying flower array data as buttons
+function displayButtons() {
     $("#buttons-view").empty();
-    // Delete the content inside the buttons-view div prior 
-
-    // Loop through the array of flowers, then generate buttons for each movie in the array
     for (var i = 0; i < flowers.length; i++) {
         var a = $("<button>");
         a.addClass("flower");
@@ -35,47 +32,15 @@ function renderButtons() {
         $("#buttons-view").append(a);
     }
 }
-renderButtons();
-// function alertFlowerName() {
-//     var flowerName = $(this).attr("data-name");
 
-//     alert(flowerName);
-// }
+// This function takes the input value and adds it to the existing array of flowers
 
-// function renderButtons() {
-
-//     // Deleting the movies prior to adding new movies
-//     // (this is necessary otherwise we will have repeat buttons)
-//     $("#buttons-view").empty();
-
-//     // Looping through the array of movies
-//     for (var i = 0; i < flowers.length; i++) {
-
-//         // Then dynamicaly generating buttons for each movie in the array
-//         // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
-//         var a = $("<button>");
-//         // Adding a class of movie to our button
-//         a.addClass("flower");
-//         // Adding a data-attribute
-//         a.attr("data-name", flowers[i]);
-//         // Providing the initial button text
-//         a.text(flowers[i]);
-//         // Adding the button to the HTML
-//         $("#buttons-view").append(a);
-//     }
-// }
+$("#add-flower").on("click", function (event) {
+    event.preventDefault();
+    var flower = $("#flower-input").val().trim();
+    flowers.push(flower);
+    displayButtons();
+});
 
 
-// // function displayGif() {
-
-
-// //     var exercise = $(this).attr("data-name");
-// //     // Example queryURL for Giphy API
-// //     var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=AO19ApHDYRwaLBR55kjmQCddKIcDrAKy";
-
-// //     $.ajax({
-// //         url: queryURL,
-// //         method: "GET"
-// //     }).then(function (response) {
-// //         console.log(response);
-// //     });
+displayButtons();
